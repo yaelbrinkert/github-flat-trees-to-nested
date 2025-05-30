c6f06457437e1dbfa77917ef6fb06e214cb11b09
Yael BRINKERT <75533817+yaelbrinkert@users.noreply.github.com>
2025-05-31 01:37:59 +0200

    init

---
+++ actions.js
@@ -0,0 +1,38 @@
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+  auth: process.env.TOKEN_GITHUB,
+});
+
+export async function getRootFoldersFromRepo() {
+  try {
+    const { data } = await octokit.request(
+      "GET /repos/{owner}/{repo}/git/trees/{tree_sha}?recursive=1",
+      {
+        owner: "yaelbrinkert",
+        repo: "fortnite-archives",
+        tree_sha: "main",
+      }
+    );
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getOnefile(itemPath) {
+  try {
+    const { data } = await octokit.request(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      {
+        owner: "yaelbrinkert",
+        repo: "fortnite-archives",
+        path: "chapter_6/season_1/33_00/33_00.jpg",
+      }
+    );
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
---
+++ transform-to-tree.js
@@ -0,0 +1,9 @@
+export function buildNested(flatTree) {
+    let nested = []
+    
+    // flatTree.map((fl))
+    if(!nested.includes(folders)) {
+        nested.push(folders)
+    }
+    return nested
+}
