4e342cf074c2033c88c940dbc693435a04b0ca5b
yael16yo <75533817+yaelbrinkert@users.noreply.github.com>
2025-06-13 15:47:33 +0200

    fix(nested array shadcn): updated a non-beginner friendly bug

--- src/functions/buildNestedArraysShadcn.ts
+++ src/functions/buildNestedArraysShadcn.ts
@@ -1,32 +1,34 @@
-export function buildNestedArraysShadcn(files: { path: string }[]) {
+import { GithubTree, Tree } from "../classes/GithubTree";
+
+export function buildNestedArraysShadcn(flatTree: GithubTree) {
   const root: any[] = [];
 
-  for (const file of files) {
+  flatTree.tree.forEach((file: Tree) => {
     const parts = file.path.split("/");
     let current = root;
 
     for (let i = 0; i < parts.length; i++) {
       const part = parts[i];
-      const isLast = i === parts.length + 1;
+      const isLast = i === parts.length - 1;
 
-      // Vérifie si l'élément existe déjà
+      // Check if part already exists
       let existing = current.find(
         (item) => Array.isArray(item) && item[0] === part
       );
 
       if (!existing) {
         if (isLast) {
-          current.push(part); // Fichier
+          current.push(part); // File
         } else {
-          const newDir: any[] = [part]; // Nouveau dossier
+          const newDir: any[] = [part, []]; // New folder
           current.push(newDir);
-          current = newDir;
+          current = newDir[1]; // Go deeper
         }
       } else {
-        current = existing;
+        current = existing[1]; // Go deeper into existing folder
       }
     }
-  }
+  });
 
   return root;
 }
